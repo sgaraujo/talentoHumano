7ae8dff3f1650a33f830f4e985e48a1da010f395
Santiago <santigarcia2321@gmail.com>
2026-02-26 21:04:03 -0500

    hola

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from "firebase/functions";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBnh9P0sI-JeSi-1lO1JDEAs9ty2V0HTf4",
+  apiKey: "AIzaSyAN_oTLg9fBYMaVgrNo0TprrHkB8V7DcHc",
   authDomain: "nelyoda.firebaseapp.com",
   projectId: "nelyoda",
   storageBucket: "nelyoda.firebasestorage.app",
-  messagingSenderId: "423942696621",
-  appId: "1:423942696621:web:62d2ce1f38dfa08de04b39"
+  messagingSenderId: "881272343488",
+  appId: "1:881272343488:web:a92d0871275dbeee071625"
 };
 
 const app = initializeApp(firebaseConfig);
